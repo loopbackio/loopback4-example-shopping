db45384188bcfb63efbb75fef34db139c4387778
virkt25 <taranveer@virk.cc>
2018-08-08 23:58:34 -0400

    chore: add license, license headers for code files
    Signed-off-by: virkt25 <taranveer@virk.cc>

--- src/application.ts
+++ src/application.ts
@@ -1,3 +1,8 @@
+// Copyright IBM Corp. 2018. All Rights Reserved.
+// Node module: @loopback/example-shopping
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
 import {ApplicationConfig} from '@loopback/core';
 import {RestApplication, RestServer, RestBindings} from '@loopback/rest';
 import {MySequence} from './sequence';
