3b610fcacd622944bae444ae19d8e17dfac40d54
virkt25 <taranveer@virk.cc>
2018-08-08 23:46:57 -0400

    feat: project scaffolding using lb4 app
    Signed-off-by: virkt25 <taranveer@virk.cc>

---
+++ test/ping.controller.acceptance.ts
@@ -0,0 +1,42 @@
+import {createClientForHandler, supertest} from '@loopback/testlab';
+import {RestServer} from '@loopback/rest';
+import {ShoppingApplication} from '../';
+
+describe('PingController', () => {
+  let app: ShoppingApplication;
+  let server: RestServer;
+  let client: supertest.SuperTest<supertest.Test>;
+
+  before(givenAnApplication);
+
+  before(givenARestServer);
+
+  before(async () => {
+    await app.boot();
+    await app.start();
+  });
+
+  before(() => {
+    client = createClientForHandler(server.requestHandler);
+  });
+
+  after(async () => {
+    await app.stop();
+  });
+
+  it('invokes GET /ping', async () => {
+    await client.get('/ping?msg=world').expect(200);
+  });
+
+  function givenAnApplication() {
+    app = new ShoppingApplication({
+      rest: {
+        port: 0,
+      },
+    });
+  }
+
+  async function givenARestServer() {
+    server = await app.getServer(RestServer);
+  }
+});
