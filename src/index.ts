d92fb37cacce37b58f763614ee13300e278b3700
virkt25 <taranveer@virk.cc>
2018-08-13 20:03:36 -0400

    chore: update files to match latest CLI template output
    Signed-off-by: virkt25 <taranveer@virk.cc>

--- src/index.ts
+++ src/index.ts
@@ -12,5 +12,10 @@ export async function main(options?: ApplicationConfig) {
   const app = new ShoppingApplication(options);
   await app.boot();
   await app.start();
+
+  const url = app.restServer.url;
+  console.log(`Server is running at ${url}`);
+  console.log(`Try ${url}/ping`);
+
   return app;
 }
