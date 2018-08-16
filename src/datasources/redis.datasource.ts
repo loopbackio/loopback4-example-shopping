6a93723eaada704541b3c1bbd392c3a10b9a688c
Raymond Feng <enjoyjava@gmail.com>
2018-08-16 10:52:55 -0700

    feat: adding artifacts for shopping cart
    Signed-off-by: Raymond Feng <enjoyjava@gmail.com>

---
+++ src/datasources/redis.datasource.ts
@@ -0,0 +1,19 @@
+// Copyright IBM Corp. 2018. All Rights Reserved.
+// Node module: @loopback/example-shopping
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+import {inject} from '@loopback/core';
+import {juggler, AnyObject} from '@loopback/repository';
+const config = require('./redis.datasource.json');
+
+export class RedisDataSource extends juggler.DataSource {
+  static dataSourceName = 'redis';
+
+  constructor(
+    @inject('datasources.config.redis', {optional: true})
+    dsConfig: AnyObject = config,
+  ) {
+    super(dsConfig);
+  }
+}
