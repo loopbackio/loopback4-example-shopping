93201921663b375120aed74456648166cf48d3d1
Raymond Feng <enjoyjava@gmail.com>
2018-08-29 12:52:39 -0700

    chore: improve scripts for docker start/stop
    Signed-off-by: Raymond Feng <enjoyjava@gmail.com>

--- bin/start-dbs.sh
+++ bin/start-dbs.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 if [ -z "$CI" ]; then
-  MONGO_CONTAINER_NAME="mongodb_c"
-  docker rm -f $MONGO_CONTAINER_NAME
+  BASE_DIR=`dirname "$0"`
+  $BASE_DIR/stop-dbs.sh
+
+  MONGO_CONTAINER_NAME="mongodb_lb4_shopping"
   docker pull mongo:latest
   docker run --name $MONGO_CONTAINER_NAME -p 27017:27017 -d mongo:latest
 
-  REDIS_CONTAINER_NAME="redis_c"
-  docker rm -f $REDIS_CONTAINER_NAME
+  REDIS_CONTAINER_NAME="redis_lb4_shopping"
   docker pull redis:latest
   docker run --name $REDIS_CONTAINER_NAME -p 6379:6379 -d redis:latest
 fi
--- bin/stop-dbs.sh
+++ bin/stop-dbs.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-MONGO_CONTAINER_NAME="mongodb_c"
-docker rm -f $MONGO_CONTAINER_NAME
+MONGO_CONTAINER_NAME="mongodb_lb4_shopping"
+REDIS_CONTAINER_NAME="redis_lb4_shopping"
+docker rm -f $MONGO_CONTAINER_NAME $REDIS_CONTAINER_NAME
 
-REDIS_CONTAINER_NAME="redis_c"
-docker rm -f $REDIS_CONTAINER_NAME
 
