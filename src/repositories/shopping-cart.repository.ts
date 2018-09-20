429a6ae072074693ab9c5cb1a1fbf7cee2d24992
Raymond Feng <enjoyjava@gmail.com>
2018-09-20 10:28:33 -0700

    feat: enable retry for addItem and add more debug info
    Signed-off-by: Raymond Feng <enjoyjava@gmail.com>

--- src/repositories/shopping-cart.repository.ts
+++ src/repositories/shopping-cart.repository.ts
@@ -8,6 +8,7 @@ import {ShoppingCart, ShoppingCartItem} from '../models/shopping-cart.model';
 import {RedisDataSource} from '../datasources/redis.datasource';
 import {inject} from '@loopback/context';
 import {promisify} from 'util';
+import {Task, retry} from '../utils/retry';
 
 export class ShoppingCartRepository extends DefaultKeyValueRepository<
   ShoppingCart
@@ -18,19 +19,32 @@ export class ShoppingCartRepository extends DefaultKeyValueRepository<
 
   /**
    * Add an item to the shopping cart with optimistic lock to allow concurrent
-   * `adding to cart` from multiple devices
+   * `adding to cart` from multiple devices. If race condition happens, it will
+   * try 10 times at an interval of 10 ms. Timeout will be reported as an error.
    *
    * @param userId User id
    * @param item Item to be added
+   * @returns A promise that's resolved with the updated ShoppingCart instance
+   *
    */
   addItem(userId: string, item: ShoppingCartItem) {
-    const addItemToCart = (cart: ShoppingCart | null) => {
-      cart = cart || new ShoppingCart({userId});
-      cart.items = cart.items || [];
-      cart.items.push(item);
-      return cart;
+    const task: Task<ShoppingCart> = {
+      run: async () => {
+        const addItemToCart = (cart: ShoppingCart | null) => {
+          cart = cart || new ShoppingCart({userId});
+          cart.items = cart.items || [];
+          cart.items.push(item);
+          return cart;
+        };
+        const result = await this.checkAndSet(userId, addItemToCart);
+        return {
+          done: result != null,
+          value: result,
+        };
+      },
+      description: `update the shopping cart for '${userId}'`,
     };
-    return this.checkAndSet(userId, addItemToCart);
+    return retry(task, {maxTries: 10, interval: 10});
   }
 
   /**
@@ -42,6 +56,10 @@ export class ShoppingCartRepository extends DefaultKeyValueRepository<
    * @param userId User id
    * @param check A function that checks the current value and produces a new
    * value. It returns `null` to abort.
+   *
+   * @returns A promise that's resolved with the updated ShoppingCart instance
+   * or with null if the transaction failed due to a race condition.
+   * See https://github.com/NodeRedis/node_redis#optimistic-locks
    */
   async checkAndSet(
     userId: string,
@@ -66,7 +84,7 @@ export class ShoppingCartRepository extends DefaultKeyValueRepository<
     if (!cart) return null;
     await execute('MULTI', []);
     await this.set(userId, cart);
-    await execute('EXEC', []);
-    return cart;
+    const result = await execute('EXEC', []);
+    return result == null ? null : cart;
   }
 }
---
+++ src/utils/retry.ts
@@ -0,0 +1,77 @@
+// Copyright IBM Corp. 2018. All Rights Reserved.
+// Node module: @loopback/example-shopping
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+import {HttpErrors} from '@loopback/rest';
+import {promisify} from 'util';
+import * as debugFactory from 'debug';
+const debug = debugFactory('loopback:example:shopping');
+
+export interface TaskStatus<T> {
+  done: boolean;
+  value?: T | null;
+}
+
+/**
+ * A task that can be retried
+ */
+export interface Task<T> {
+  run(): Promise<TaskStatus<T>>;
+  description: string;
+}
+
+/**
+ * Options for retry
+ */
+export interface RetryOptions {
+  /**
+   * Maximum number of tries including the first run.
+   */
+  maxTries?: number;
+  /**
+   * Milliseconds to wait after each try
+   */
+  interval?: number;
+}
+
+/**
+ * Retry a task for number of times with the given interval in ms
+ * @param task Task object {run, description}
+ * @param maxTries Maximum number of tries (including the first run),
+ * default to 10
+ * @param interval Milliseconds to wait after each try, default to 100ms
+ */
+export async function retry<T>(
+  task: Task<T>,
+  {maxTries = 10, interval = 100}: RetryOptions = {},
+): Promise<T> {
+  if (maxTries < 1) maxTries = 1;
+  let triesLeft = maxTries;
+  while (true) {
+    debug(
+      'Try %s (%d/%d)',
+      task.description,
+      maxTries - triesLeft + 1,
+      maxTries,
+    );
+    const status = await task.run();
+    if (status.done) return status.value!;
+    if (--triesLeft > 0) {
+      debug('Wait for %d ms', interval);
+      await sleep(interval);
+    } else {
+      // No more retries, timeout
+      const msg = `Failed to ${task.description} after ${maxTries *
+        interval} ms`;
+      debug('%s', msg);
+      throw new HttpErrors.RequestTimeout(msg);
+    }
+  }
+}
+
+/**
+ * Sleep for the given milliseconds
+ * @param ms Number of milliseconds to wait
+ */
+export const sleep = promisify(setTimeout); // (ms: number) => Promise<void>
