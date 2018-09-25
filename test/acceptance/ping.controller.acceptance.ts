c5d858dcff9bfa3243d6f4d5f6984c1fc70094be
Raymond Feng <enjoyjava@gmail.com>
2018-09-25 07:36:16 -0700

    chore: clean up ping controller
    Signed-off-by: Raymond Feng <enjoyjava@gmail.com>

--- test/acceptance/ping.controller.acceptance.ts
+++ test/acceptance/ping.controller.acceptance.ts
@@ -34,45 +34,6 @@ describe('PingController', () => {
     expect(res.body).to.containEql({greeting: 'Hello from LoopBack'});
   });
 
-  it('exposes OpenAPI spec at /openapi.json', async () => {
-    const res = await client.get('/openapi.json').expect(200);
-    expect(res.body).to.containDeep({
-      paths: {
-        '/ping': {
-          get: {
-            'x-controller-name': 'PingController',
-            'x-operation-name': 'ping',
-            tags: ['PingController'],
-            responses: {
-              '200': {
-                description: 'Ping Response',
-                content: {
-                  'application/json': {
-                    schema: {
-                      type: 'object',
-                      properties: {
-                        greeting: {type: 'string'},
-                        date: {type: 'string'},
-                        url: {type: 'string'},
-                        headers: {
-                          type: 'object',
-                          patternProperties: {
-                            '^.*$': {type: 'string'},
-                          },
-                          additionalProperties: false,
-                        },
-                      },
-                    },
-                  },
-                },
-              },
-            },
-          },
-        },
-      },
-    });
-  });
-
   function givenAnApplication() {
     app = new ShoppingApplication({
       rest: {
