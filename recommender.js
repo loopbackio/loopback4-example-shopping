20756a462e775c7e63c83f8c608f2fb40d411adf
Raymond Feng <enjoyjava@gmail.com>
2018-11-01 18:14:30 -0700

    chore: upgrade to LoopBack4@1.x
    Signed-off-by: Raymond Feng <enjoyjava@gmail.com>

--- recommender.js
+++ recommender.js
@@ -3,13 +3,8 @@
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
-const distUtil = require('@loopback/dist-util');
 const path = require('path');
 
-const app = require(path.join(
-  __dirname,
-  distUtil.getDist(__dirname),
-  '/recommender',
-));
+const app = require('./dist/recommender');
 
 module.exports = app.createRecommendationServer();
