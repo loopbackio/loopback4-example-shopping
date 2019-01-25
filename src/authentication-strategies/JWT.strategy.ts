7dbe01528705f4e5ab5160ab79dd4365cf6ebd68
jannyHou <juehou@ca.ibm.com>
2019-01-25 15:39:06 -0500

    refactor: extract decode user to util
    Signed-off-by: jannyHou <juehou@ca.ibm.com>

--- src/authentication-strategies/JWT.strategy.ts
+++ src/authentication-strategies/JWT.strategy.ts
@@ -3,15 +3,12 @@
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
-const jwt = require('jsonwebtoken');
-import {promisify} from 'util';
-const verifyAsync = promisify(jwt.verify);
 // Consider turn it to a binding
 const SECRET = 'secretforjwt';
 import {Request, HttpErrors} from '@loopback/rest';
 import {UserProfile} from '@loopback/authentication';
-import * as _ from 'lodash';
 import {AuthenticationStrategy} from './authentication.strategy';
+import {decodeAccessToken} from '../utils/user.authentication';
 
 export class JWTStrategy implements AuthenticationStrategy {
   async authenticate(request: Request): Promise<UserProfile | undefined> {
@@ -23,10 +20,7 @@ export class JWTStrategy implements AuthenticationStrategy {
     }
 
     try {
-      const decoded = await verifyAsync(token, SECRET);
-      let user = _.pick(decoded, ['id', 'email', 'firstName']);
-      (user as UserProfile).name = user.firstName;
-      delete user.firstName;
+      const user = await decodeAccessToken(token, SECRET);
       return user;
     } catch (err) {
       Object.assign(err, {
