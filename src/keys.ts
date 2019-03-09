021a3098020f9f773cda8a3c5daaf0059637d9de
Diana Lau <dhmlau@ca.ibm.com>
2019-03-09 17:42:21 -0500

    chore: update copyright header
    Signed-off-by: Diana Lau <dhmlau@ca.ibm.com>

--- src/keys.ts
+++ src/keys.ts
@@ -1,3 +1,8 @@
+// Copyright IBM Corp. 2019. All Rights Reserved.
+// Node module: loopback4-example-shopping
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
 import {BindingKey} from '@loopback/context';
 import {JWTAuthenticationService} from './services/JWT.authentication.service';
 import {JWTStrategy} from './authentication-strategies/JWT.strategy';
