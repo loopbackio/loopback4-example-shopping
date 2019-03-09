245c156c00f065cc26ee95c9b5599c2e7c71ee31
Diana Lau <dhmlau@ca.ibm.com>
2019-03-09 17:42:21 -0500

    chore: update copyright header
    Signed-off-by: Diana Lau <dhmlau@ca.ibm.com>

--- src/services/hash.password.bcryptjs.ts
+++ src/services/hash.password.bcryptjs.ts
@@ -1,3 +1,8 @@
+// Copyright IBM Corp. 2019. All Rights Reserved.
+// Node module: loopback4-example-shopping
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
 import {genSalt, hash} from 'bcryptjs';
 import {compare} from 'bcryptjs';
 import {inject} from '@loopback/core';
