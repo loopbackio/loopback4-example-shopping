41964e7740f5666f35f4be04f536e43793b4e0fb
Raymond Feng <enjoyjava@gmail.com>
2019-04-19 12:48:03 -0700

    refactor: move recommender server to a package
    Signed-off-by: Raymond Feng <enjoyjava@gmail.com>

---
+++ packages/recommender/src/index.ts
@@ -0,0 +1,7 @@
+// Copyright IBM Corp. 2018. All Rights Reserved.
+// Node module: loopback4-example-recommender
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+export * from './mock-recommendation-app';
+export * from '@loopback/http-server';
