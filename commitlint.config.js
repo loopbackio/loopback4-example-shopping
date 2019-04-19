ee557f0dee0a9a5bd30e1f6b8f63b467d23162ca
Raymond Feng <enjoyjava@gmail.com>
2019-04-19 12:48:44 -0700

    refactor: convert the repo into a monorepo managed by lerna
    Signed-off-by: Raymond Feng <enjoyjava@gmail.com>

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,7 +4,10 @@
 // License text available at https://opensource.org/licenses/MIT
 
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
+  extends: [
+    '@commitlint/config-conventional',
+    '@commitlint/config-lerna-scopes',
+  ],
   rules: {
     'header-max-length': [2, 'always', 100],
     'body-leading-blank': [2, 'always'],
