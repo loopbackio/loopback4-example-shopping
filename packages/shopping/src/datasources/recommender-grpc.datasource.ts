3e06d98c941c81d48f460115e186dc6638372640
Raymond Feng <enjoyjava@gmail.com>
2019-09-01 23:15:55 -0700

    feat: improve datasources for configuration and lifecycle
    - make datasources aware of kubernetes deployment
    - detect k8s and use environment variables to set up datasources
    - enable lifecycle support for datasources
    
    Signed-off-by: Raymond Feng <enjoyjava@gmail.com>

--- packages/shopping/src/datasources/recommender-grpc.datasource.ts
+++ packages/shopping/src/datasources/recommender-grpc.datasource.ts
@@ -1,16 +1,31 @@
 import {inject} from '@loopback/core';
-import {juggler} from '@loopback/repository';
-import * as config from './recommender-grpc.datasource.json';
+import {juggler, AnyObject} from '@loopback/repository';
 import * as path from 'path';
+
+const config = require('./recommender-grpc.datasource.json');
+
+function updateConfig(dsConfig: AnyObject) {
+  if (
+    process.env.KUBERNETES_SERVICE_HOST &&
+    process.env.RECOMMENDER_SERVICE_HOST
+  ) {
+    dsConfig.url = `${process.env.RECOMMENDER_SERVICE_HOST}:${+process.env
+      .RECOMMENDER_SERVICE_PORT_GRPC!}`;
+  }
+  return dsConfig;
+}
 
 export class RecommenderGrpcDataSource extends juggler.DataSource {
   static dataSourceName = 'recommender_grpc';
 
   constructor(
     @inject('datasources.config.recommender_grpc', {optional: true})
-    dsConfig: object = config,
+    dsConfig: AnyObject = config,
   ) {
-    super({...dsConfig, spec: RecommenderGrpcDataSource.getProtoFile()});
+    super({
+      ...updateConfig(dsConfig),
+      spec: RecommenderGrpcDataSource.getProtoFile(),
+    });
   }
 
   private static getProtoFile() {
