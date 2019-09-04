133aab43f8aa04371d806081cf189c4efe86affe
Raymond Feng <enjoyjava@gmail.com>
2019-09-04 12:40:51 -0700

    chore: fix eslint violations
    Signed-off-by: Raymond Feng <enjoyjava@gmail.com>

--- packages/recommender/src/mock-recommendation-app.ts
+++ packages/recommender/src/mock-recommendation-app.ts
@@ -7,10 +7,7 @@ import * as express from 'express';
 const recommendations = require('../data/recommendations.json');
 import {HttpServer} from '@loopback/http-server';
 
-export function createRecommendationServer(
-  port: number = 3001,
-  host: string = '127.0.0.1',
-) {
+export function createRecommendationServer(port = 3001, host = '127.0.0.1') {
   const app = express();
 
   app.get('/:userId', (req: express.Request, res: express.Response) => {
@@ -20,7 +17,7 @@ export function createRecommendationServer(
   return new HttpServer(app, {port, host});
 }
 
-export async function main(port: number = 3001) {
+export async function main(port = 3001) {
   const server = createRecommendationServer(port);
   await server.start();
   console.log('Recommendation server is running at ' + server.url + '.');
