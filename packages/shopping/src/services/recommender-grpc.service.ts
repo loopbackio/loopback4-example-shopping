d532a508bfaf8d3816b737daae47f13d5eaf24fe
Raymond Feng <enjoyjava@gmail.com>
2019-10-16 15:10:39 -0700

    feat: use RECOMMENDER_PROTOCOL to select REST or gRPC
    Signed-off-by: Raymond Feng <enjoyjava@gmail.com>

--- packages/shopping/src/services/recommender-grpc.service.ts
+++ packages/shopping/src/services/recommender-grpc.service.ts
@@ -1,7 +1,8 @@
 import {getService} from '@loopback/service-proxy';
-import {inject, Provider} from '@loopback/core';
+import {inject, Provider, bind} from '@loopback/core';
 import {RecommenderGrpcDataSource} from '../datasources';
 import {Product} from '../models';
+import {RecommenderService, recommender} from './recommender.service';
 
 export interface RecommenderGrpc {
   // this is where you define the Node.js methods that will be
@@ -10,14 +11,26 @@ export interface RecommenderGrpc {
   recommend(req: {userId: string}): Promise<{products: Product[]}>;
 }
 
-export class RecommenderGrpcProvider implements Provider<RecommenderGrpc> {
+/**
+ * gRPC based recommender service
+ */
+@bind(recommender('grpc'))
+export class RecommenderGrpcServiceProvider
+  implements Provider<RecommenderService> {
   constructor(
     // recommender must match the name property in the datasource json file
     @inject('datasources.recommender_grpc')
     protected dataSource: RecommenderGrpcDataSource = new RecommenderGrpcDataSource(),
   ) {}
 
-  value(): Promise<RecommenderGrpc> {
-    return getService(this.dataSource);
+  async value(): Promise<RecommenderService> {
+    const grpcService = await getService<RecommenderGrpc>(this.dataSource);
+    const service: RecommenderService = {
+      getProductRecommendations: async (userId: string) => {
+        const res = await grpcService.recommend({userId});
+        return res.products;
+      },
+    };
+    return service;
   }
 }
