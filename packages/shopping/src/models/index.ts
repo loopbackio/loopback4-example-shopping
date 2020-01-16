50abcad8d654679d75ada2e201ebf18ed5e8fa28
Hage Yaapa <hage.yaapa@in.ibm.com>
2020-01-16 16:20:19 +0530

    feat: back-end changes
    Back-end changes for the website.
    Signed-off-by: Hage Yaapa <hage.yaapa@in.ibm.com>

--- packages/shopping/src/models/index.ts
+++ packages/shopping/src/models/index.ts
@@ -7,5 +7,5 @@ export * from './user.model';
 export * from './shopping-cart-item.model';
 export * from './shopping-cart.model';
 export * from './order.model';
-export * from './product.model';
 export * from './user-credentials.model';
+export * from './product.model';
