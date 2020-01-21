605832bf6e612155dac5083934209a294725432b
jannyHou <juehou@ca.ibm.com>
2020-01-21 12:21:57 -0500

    refactor: rename security scheme name
    Signed-off-by: jannyHou <juehou@ca.ibm.com>

--- packages/shopping/src/utils/security-spec.ts
+++ packages/shopping/src/utils/security-spec.ts
@@ -5,12 +5,12 @@
 
 import {SecuritySchemeObject, ReferenceObject} from '@loopback/openapi-v3';
 
-export const OPERATION_SECURITY_SPEC = [{bearerAuth: []}];
+export const OPERATION_SECURITY_SPEC = [{jwt: []}];
 export type SecuritySchemeObjects = {
   [securityScheme: string]: SecuritySchemeObject | ReferenceObject;
 };
 export const SECURITY_SCHEME_SPEC: SecuritySchemeObjects = {
-  bearerAuth: {
+  jwt: {
     type: 'http',
     scheme: 'bearer',
     bearerFormat: 'JWT',
