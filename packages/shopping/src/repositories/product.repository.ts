c5361df2ba78d045a9f736b809b68786ef82d08c
Diana Lau <dhmlau@ca.ibm.com>
2020-02-08 17:15:32 -0500

    chore: update copyrights year
    Signed-off-by: Diana Lau <dhmlau@ca.ibm.com>

--- packages/shopping/src/repositories/product.repository.ts
+++ packages/shopping/src/repositories/product.repository.ts
@@ -1,3 +1,8 @@
+// Copyright IBM Corp. 2019,2020. All Rights Reserved.
+// Node module: loopback4-example-shopping
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
 import {DefaultCrudRepository} from '@loopback/repository';
 import {Product, ProductRelations} from '../models';
 import {MongoDataSource} from '../datasources';
--- packages/shopping/src/services/recommender-grpc.service.ts
+++ packages/shopping/src/services/recommender-grpc.service.ts
@@ -1,3 +1,8 @@
+// Copyright IBM Corp. 2019. All Rights Reserved.
+// Node module: loopback4-example-shopping
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
 import {getService} from '@loopback/service-proxy';
 import {inject, Provider, bind} from '@loopback/core';
 import {RecommenderGrpcDataSource} from '../datasources';
