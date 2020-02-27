1bdb065e1380e730f14934cc2239c3f5a7b7ccc9
derdeka <derdeka@blackslash.de>
2020-02-27 17:45:28 +0100

    feat: import ApiModule into angular
    Signed-off-by: derdeka <derdeka@blackslash.de>

--- packages/angular/src/app/app.module.ts
+++ packages/angular/src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+import { Configuration as ApiConfiguration, ApiModule } from './shared/sdk';
+
+export function apiConfigurationFactory(): ApiConfiguration {
+  return new ApiConfiguration({
+    basePath: environment.apiBaseUrl,
+  });
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    HttpClientModule,
+    ApiModule.forRoot(apiConfigurationFactory),
     BrowserModule,
     AppRoutingModule
   ],
--- packages/angular/src/environments/environment.ts
+++ packages/angular/src/environments/environment.ts
@@ -3,6 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  apiBaseUrl: 'http://localhost:3000',
   production: false
 };
 
