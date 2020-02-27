8eb2ed7832a6a8f0013158fd8f61878f563bc95d
derdeka <derdeka@blackslash.de>
2020-02-27 17:07:07 +0100

    feat: import ApiModule into angular
    Signed-off-by: derdeka <derdeka@blackslash.de>

--- packages/angular/src/app/app.module.ts
+++ packages/angular/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+import { ApiModule } from './shared/sdk/api.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    HttpClientModule,
+    ApiModule.forRoot({ rootUrl: environment.apiBaseUrl }),
     BrowserModule,
     AppRoutingModule
   ],
