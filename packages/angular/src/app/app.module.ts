22d96adaa6da1ca03bad8aab6bce8d05c36d029c
derdeka <derdeka@blackslash.de>
2020-02-28 11:01:38 +0100

    feat: add bootstrap via ng-bootstrap
    Signed-off-by: derdeka <derdeka@blackslash.de>

--- packages/angular/src/app/app.module.ts
+++ packages/angular/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    NgbModule,
     BrowserModule,
     AppRoutingModule
   ],
