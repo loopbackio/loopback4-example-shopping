f5a3b498d80bbfc662f9205bb0aa45d4605b36bd
mrmodise <modisemorebodi@gmail.com>
2020-12-19 07:48:37 +0200

    feat: remove index.js and add optional rest configs
    Signed-off-by: mrmodise <modisemorebodi@gmail.com>

--- packages/shopping/src/index.ts
+++ packages/shopping/src/index.ts
@@ -7,6 +7,8 @@ import {ShoppingApplication} from './application';
 import {ApplicationConfig} from '@loopback/core';
 export {ShoppingApplication, PackageInfo, PackageKey} from './application';
 
+export * from './application';
+
 export async function main(options?: ApplicationConfig) {
   const app = new ShoppingApplication(options);
 
@@ -18,4 +20,41 @@ export async function main(options?: ApplicationConfig) {
   console.log(`Try ${url}/ping`);
 
   return app;
+}
+
+if (require.main === module) {
+  const config = {
+    rest: {
+      port: +(process.env.PORT ?? 3000),
+      protocol: 'http',
+      key: '',
+      cert: '',
+      host: process.env.HOST,
+      ciphers: [],
+      cors: {
+        origin: [],
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        preflightContinue: false,
+        optionsSuccessStatus: 204,
+        maxAge: 86400,
+        credentials: true,
+      },
+      // The `gracePeriodForClose` provides a graceful close for http/https
+      // servers with keep-alive clients. The default value is `Infinity`
+      // (don't force-close). If you want to immediately destroy all sockets
+      // upon stop, set its value to `0`.
+      // See https://www.npmjs.com/package/stoppable
+      gracePeriodForClose: 5000, // 5 seconds
+      openApiSpec: {
+        disabled: false,
+        // useful when used with OpenAPI-to-GraphQL to locate your application
+        setServersFromRequest: true,
+      },
+    },
+  };
+  // Run the application
+  main(config).catch(err => {
+    console.error('Cannot start the application.', err);
+    process.exit(1);
+  });
 }
